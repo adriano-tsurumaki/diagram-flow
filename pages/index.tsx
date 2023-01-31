b01d305a1e4204cd34aa434d2e9278feccffdc32
Adriano Tsurumaki <adriano.tsurumaki@havan.com.br>
2023-01-31 14:41:23 -0300

    create layout

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,24 @@
 import Flow from "@/components/flow";
+import { ReactFlowProvider } from "reactflow";
+import SideBarContent from "@/components/sidebar-content";
+import SideBarProps from "@/components/sidebar-props";
+import TopBar from "@/components/topbar";
 
 export default function Home() {
   return (
     <div>
-      <Flow />
+      <ReactFlowProvider>
+        <div className="flex flex-col h-full">
+          <div className="flex-none">
+            <TopBar />
+          </div>
+          <div className="flex-1 min-h-0 flex flex-row">
+            <SideBarContent />
+            <Flow />
+            <SideBarProps />
+          </div>
+        </div>
+      </ReactFlowProvider>
     </div>
   );
 }
